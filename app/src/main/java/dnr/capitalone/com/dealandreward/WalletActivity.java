b76c940bd0632eb57687007669d0775be7e1ec91
Richard Yan <yan37@illinois.edu>
2015-06-22 22:54:53 -0500

    Wallet

--- app/src/main/java/dnr/capitalone/com/dealandreward/WalletActivity.java
+++ app/src/main/java/dnr/capitalone/com/dealandreward/WalletActivity.java
@@ -1,17 +1,90 @@
 package dnr.capitalone.com.dealandreward;
 
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.ScaleDrawable;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 
 public class WalletActivity extends ActionBarActivity {
 
+    Drawable drawable;
+    Button button;
+    ScaleDrawable sd;
+    ImageButton imgButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_wallet);
+        setContentView(R.layout.activity_coupon_wallet);
+        //setContentView(R.layout.activity_wallet);
+
+        drawable = getResources().getDrawable(R.drawable.hm_icon);
+        drawable.setBounds(0, 0, (int) (drawable.getIntrinsicWidth() * 0.25),
+                (int) (drawable.getIntrinsicHeight() * 0.25));
+        sd = new ScaleDrawable(drawable, 0, 70, 70);
+        button = (Button) findViewById(R.id.hmButton);
+        button.setCompoundDrawables(sd.getDrawable(), null, null, null); //set drawableLeft for example
+
+        drawable = getResources().getDrawable(R.drawable.starbucks_icon);
+        drawable.setBounds(0, 0, (int) (drawable.getIntrinsicWidth() * 0.25),
+                (int) (drawable.getIntrinsicHeight() * 0.25));
+        sd = new ScaleDrawable(drawable, 0, 70, 70);
+        button = (Button) findViewById(R.id.starbucksButton);
+        button.setCompoundDrawables(sd.getDrawable(), null, null, null); //set drawableLeft for example
+
+        drawable = getResources().getDrawable(R.drawable.walmart_icon);
+        drawable.setBounds(0, 0, (int) (drawable.getIntrinsicWidth() * 0.25),
+                (int) (drawable.getIntrinsicHeight() * 0.25));
+        sd = new ScaleDrawable(drawable, 0, 70, 70);
+        button = (Button) findViewById(R.id.walmartButton);
+        button.setCompoundDrawables(sd.getDrawable(), null, null, null); //set drawableLeft for example
+
+        drawable = getResources().getDrawable(R.drawable.pizzahut_icon);
+        drawable.setBounds(0, 0, (int) (drawable.getIntrinsicWidth() * 0.25),
+                (int) (drawable.getIntrinsicHeight() * 0.25));
+        sd = new ScaleDrawable(drawable, 0, 70, 70);
+        button = (Button) findViewById(R.id.pizzahutButton);
+        button.setCompoundDrawables(sd.getDrawable(), null, null, null); //set drawableLeft for example
+
+        drawable = getResources().getDrawable(R.drawable.bestbuy_icon);
+        drawable.setBounds(0, 0, (int) (drawable.getIntrinsicWidth() * 0.25),
+                (int) (drawable.getIntrinsicHeight() * 0.25));
+        sd = new ScaleDrawable(drawable, 0, 70, 70);
+        button = (Button) findViewById(R.id.bestbuyButton);
+        button.setCompoundDrawables(sd.getDrawable(), null, null, null); //set drawableLeft for example
+
+
+        imgButton =(ImageButton)findViewById(R.id.icon1);
+        imgButton.setOnTouchListener(new ButtonHighlighterOnTouchListener(imgButton));
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+              /*  Toast.makeText(getApplicationContext(), "You download is resumed", Toast.LENGTH_LONG).show();*/
+
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_SUBJECT, "Sharing URL");
+                i.putExtra(Intent.EXTRA_TEXT, "http://www.url.com");
+                startActivity(Intent.createChooser(i, "Share URL"));
+            }
+        });
+
+        imgButton = (ImageButton)findViewById(R.id.homeButton);
+        imgButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), dealMainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
     @Override
